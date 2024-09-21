c66b708ff5cbc91c726120976b12f4941e9b8071
werter <arslanagica504@gmail.com>
2024-09-21 13:21:57 +0200

    project karten werden gennerriert

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,65 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
   });
+
+
+var html="";
+const preElement = document.getElementById('projekte-columns');
+
+
+
+fetch("src/json/projecte.json")
+  .then(response => response.json())
+  .then(json => {
+    
+    json.forEach(obj => {
+
+      var top = `
+        <div class="column is-12-mobile is-7-tablet is-4-desktop is-3-fullhd is-flex">
+          <div class="card min-hight-100">
+            <div class="card-header px-2 pt-2">
+              <div class="card-header-title">${obj.name}</div>
+              <div class="subtitle">${obj.year}</div>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                ${obj.description}
+              </div>
+          `
+      var github = "";
+      if (obj.github) {
+        var github = `
+                <br>
+                <div class="buttons down-left ml-2 mb-2">
+                  <button class="button" onclick="window.location.href='${obj.github}';">
+                    <span class="NerdFont" > GitHub</span>
+                  </button>
+                </div>
+            `;
+      }
+      
+      var tags = '<div class="tags down-right mb-2 mr-2">';
+      obj.tags.forEach(element => {
+        console.log(element.tag);
+        tags+=`<p class="tag is-light is-primary">${element.tag}</p>`;
+      });
+      
+    var buttom = `
+              </div>
+              
+            </div>
+
+          </div>
+        </div>
+      `;
+   //   console.log(html)
+      html += top + github + tags + buttom;
+      
+    });
+    
+
+    preElement.innerHTML=html;
+    
+    console.log(html)
+  });
+  
